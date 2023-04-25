d00450a1d746077a94e740502c3a6d40fcf5b8b7
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-04-24 20:21:12 -0600

    User can play magic number gae

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 11);
+
+        while (true)
+        {
+            Console.Write("Please guess the magic number: ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number!");
+                break;
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Guess higher next time.");
+            }
+            else
+            {
+                Console.WriteLine("Guess lower next time.");
+            }
+        }
     }
 }
