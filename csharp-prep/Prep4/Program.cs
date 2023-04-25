e4502f8cbf7465caabd0e0ec5c4a59723a97f5e2
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-04-24 22:26:01 -0600

    User can input a series of numbers and get sum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,33 +1,35 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         List<int> numbers = new List<int>();
-        while (true)
-        {
-            Console.Write("Enter a number (0 to stop): ");
-            int num = int.Parse(Console.ReadLine());
-            if (num == 0)
-            {
-                break;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int input;
+        do {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0) {
+                numbers.Add(input);
             }
-            numbers.Add(num);
+        } while (input != 0);
+        
+        int sum = 0;
+        foreach (int number in numbers) {
+            sum += number;
         }
-
-        List<int> positives = numbers.Where(num => num > 0).ToList();
-        int? closestToZero = positives.Any() ? positives.OrderBy(num => Math.Abs(num)).First() : null;
-
-        List<int> sortedNumbers = numbers.OrderBy(num => num).ToList();
-
-        Console.WriteLine("Numbers: " + string.Join(", ", numbers));
-        Console.WriteLine("Total: " + numbers.Sum());
-        Console.WriteLine("Average: " + numbers.Average());
-        Console.WriteLine("Maximum: " + numbers.Max());
-        Console.WriteLine("Closest to zero: " + closestToZero);
-        Console.WriteLine("Sorted numbers: " + string.Join(", ", sortedNumbers));
+        double average = (double)sum / numbers.Count;
+        int max = numbers[0];
+        foreach (int number in numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + max);
     }
 }
