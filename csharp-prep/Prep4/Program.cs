d4183af7f944d3cda4a5e35a8e0c26e6b04d5641
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-04-24 21:12:35 -0600

    User can create list and sum list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.Write("Enter a number (0 to stop): ");
+            int num = int.Parse(Console.ReadLine());
+            if (num == 0)
+            {
+                break;
+            }
+            numbers.Add(num);
+        }
+
+        List<int> positives = numbers.Where(num => num > 0).ToList();
+        int? closestToZero = positives.Any() ? positives.OrderBy(num => Math.Abs(num)).First() : null;
+
+        List<int> sortedNumbers = numbers.OrderBy(num => num).ToList();
+
+        Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+        Console.WriteLine("Total: " + numbers.Sum());
+        Console.WriteLine("Average: " + numbers.Average());
+        Console.WriteLine("Maximum: " + numbers.Max());
+        Console.WriteLine("Closest to zero: " + closestToZero);
+        Console.WriteLine("Sorted numbers: " + string.Join(", ", sortedNumbers));
     }
 }
