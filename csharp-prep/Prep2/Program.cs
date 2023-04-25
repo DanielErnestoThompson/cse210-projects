bca76504410d959318ed32f5cd8162bae69dee96
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-04-24 20:04:45 -0600

    User can now enter grade %

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    static void Main() {
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+        if (grade >= 90) {
+            letter = "A";
+        } else if (grade >= 80) {
+            letter = "B";
+        } else if (grade >= 70) {
+            letter = "C";
+        } else if (grade >= 60) {
+            letter = "D";
+        } else {
+            letter = "F";
+        }
+
+        string sign;
+        int lastDigit = grade % 10;
+        if (lastDigit >= 7 && letter != "F") {
+            sign = "+";
+        } else if (lastDigit < 3 && letter != "F") {
+            sign = "-";
+        } else {
+            sign = "";
+        }
+
+        Console.WriteLine("Your letter grade is: " + letter + sign);
+
+        if (grade >= 70) {
+            Console.WriteLine("Congratulations, you passed the class!");
+        } else {
+            Console.WriteLine("Keep working hard and try again next time.");
+        }
     }
 }
