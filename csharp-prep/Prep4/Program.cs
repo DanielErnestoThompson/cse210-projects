ebdd46831cb170e0409ef6b5e31ac155988e309c
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-04-25 23:48:10 -0600

    chgs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,36 +1,61 @@
 using System;
 using System.Collections.Generic;
 
-class Program {
-    static void Main(string[] args) {
+class Program
+{
+    static void Main(string[] args)
+    {
         List<int> numbers = new List<int>();
         
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-        
-        int input;
-        do {
-            Console.Write("Enter number: ");
-            input = int.Parse(Console.ReadLine());
-            if (input != 0) {
-                numbers.Add(input);
+        // Please note we could use a do-while loop here instead
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            // Only add the number to the list if it is not 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
             }
-        } while (input != 0);
-        
+        }
+
+        // Part 1: Compute the sum
         int sum = 0;
-        foreach (int number in numbers) {
+        foreach (int number in numbers)
+        {
             sum += number;
         }
-        double average = (double)sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Part 2: Compute the average
+        // Notice that we first cast the sum variable to be a float. Otherwise, because
+        // both the sum and the count are integers, the computer will do integer division
+        // and I will not get a decimal value (even though it puts the result into a float variable).
+
+        // By making one of the variables a float first, the computer knows that it has to
+        // do the floating point division, and we get the decimal value that we expect.
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        // There are several ways to do this, such as sorting the list
+        
         int max = numbers[0];
-        foreach (int number in numbers) {
-            if (number > max) {
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
                 max = number;
             }
         }
-        
-        Console.WriteLine("The sum is: " + sum);
-        Console.WriteLine("The average is: " + average);
-        Console.WriteLine("The largest number is: " + max);
-        Console.ReadLine();
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
