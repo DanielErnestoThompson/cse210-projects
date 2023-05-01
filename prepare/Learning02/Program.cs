57462ac70dddb71373425a3b57f44f622d40f6c4
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-05-01 16:38:12 -0600

    added abstraction code to lesson 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Job
 {
-    static void Main(string[] args)
+    private string _company;
+    private string _jobTitle;
+    private int _startYear;
+    private int _endYear;
+
+    public Job(string company, string jobTitle, int startYear, int endYear)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+    }
+
+    public string GetJobInfo()
+    {
+        return $"{_jobTitle} ({_company}) {_startYear}-{_endYear}";
+    }
+}
+
+public class Resume
+{
+    private string _name;
+    private List<Job> _jobs;
+
+    public Resume(string name, List<Job> jobs)
+    {
+        _name = name;
+        _jobs = jobs;
+    }
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}\n");
+        Console.WriteLine("Work Experience:\n");
+        foreach (Job job in _jobs)
+        {
+            Console.WriteLine(job.GetJobInfo());
+        }
     }
 }
