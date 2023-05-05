2a90e5fe9db87e904cc57f5d9b791945c6461009
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-05-05 15:52:57 -0600

    add journal C# code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,160 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class JournalEntry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+
+    public JournalEntry(string prompt, string response, string date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+}
+
+class Journal
+{
+    private List<JournalEntry> _entries;
+
+    public Journal()
+    {
+        _entries = new List<JournalEntry>();
+    }
+
+    public void AddEntry(JournalEntry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in _entries)
+        {
+            Console.WriteLine($"Date: {entry.Date}\nPrompt: {entry.Prompt}\nResponse: {entry.Response}\n");
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in _entries)
+            {
+                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] parts = line.Split('|');
+                AddEntry(new JournalEntry(parts[1], parts[2], parts[0]));
+            }
+        }
+    }
+}
 
 class Program
 {
+    private static List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        int choice;
+
+        do
+        {
+            choice = DisplayMenu();
+
+            switch (choice)
+            {
+                case 1:
+                    WriteNewEntry(journal);
+                    break;
+                case 2:
+                    DisplayJournal(journal);
+                    break;
+                case 3:
+                    SaveJournal(journal);
+                    break;
+                case 4:
+                    LoadJournal(journal);
+                    break;
+            }
+        } while (choice != 5);
+    }
+
+    static int DisplayMenu()
+    {
+        Console.WriteLine("1. Write a new entry");
+        Console.WriteLine("2. Display the journal");
+        Console.WriteLine("3. Save the journal to a file");
+        Console.WriteLine("4. Load the journal from a file");
+        Console.WriteLine("5. Exit");
+        Console.Write("Choose an option: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        int index = rand.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    static void WriteNewEntry(Journal journal)
+    {
+        string prompt = GetRandomPrompt();
+        Console.WriteLine(prompt);
+        string response = Console.ReadLine();
+        string date = DateTime.Now.ToString("yyyy-MM-dd");
+        journal.AddEntry(new JournalEntry(prompt, response, date));
+    }
+
+    static void DisplayJournal(Journal journal)
+    {
+        journal.DisplayEntries();
+    }
+
+    static void SaveJournal(Journal journal)
+    {
+        Console.Write("Enter the filename to save: ");
+        string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+    }
+
+    static void LoadJournal(Journal journal)
+    {
+        Console.Write("Enter the filename to load: ");
+        string filename = Console.ReadLine();
+        try
+        {
+            journal.LoadFromFile(filename);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error loading file: " + ex.Message);
+        }
     }
 }
+
