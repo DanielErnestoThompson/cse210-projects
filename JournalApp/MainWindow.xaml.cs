83b2010d644f328cfe801d3d3a45f73689999baa
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-05-11 20:07:30 -0600

    changes

--- JournalApp/MainWindow.xaml.cs
+++ JournalApp/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using IOPath = System.IO.Path;
 using System.Windows;
+using System.Reflection;
+using Microsoft.Win32.TaskScheduler;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace JournalApp
 {
