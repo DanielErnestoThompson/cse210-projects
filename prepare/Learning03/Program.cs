893d066e3cae8bbd6991cd8c255bfa6db243e0a4
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-05-15 03:30:48 -0600

    changes

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,50 +1,23 @@
 using System;
 
-public class Fraction
+class Program
 {
-    private int _numerator; // top number
-    private int _denominator; // bottom number
-
-    public Fraction() // Constructor that initializes to 1/1
+    static void Main(string[] args)
     {
-        _numerator = 1;
-        _denominator = 1;
-    }
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
 
-    public Fraction(int numerator) // Constructor that initializes to numerator/1
-    {
-        _numerator = numerator;
-        _denominator = 1;
-    }
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
 
-    public Fraction(int numerator, int denominator) // Constructor that initializes to numerator/denominator
-    {
-        _numerator = numerator;
-        _denominator = denominator;
-    }
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
 
-    // Getters and Setters
-    public int Numerator
-    {
-        get { return _numerator; }
-        set { _numerator = value; }
-    }
-
-    public int Denominator
-    {
-        get { return _denominator; }
-        set { _denominator = value; }
-    }
-
-    // Method to get the fraction as a string
-    public string GetFractionString()
-    {
-        return $"{_numerator}/{_denominator}";
-    }
-
-    // Method to get the decimal value of the fraction
-    public double GetDecimalValue()
-    {
-        return (double)_numerator / _denominator;
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
