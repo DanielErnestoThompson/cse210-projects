ec6cd9172573ed1027bbd0a953c6c6465c9c4ea9
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-05-26 13:28:02 -0600

    changes (add main)

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,3 +1,107 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        ScriptureLibrary library = new ScriptureLibrary();
+
+        Scripture scripture = library.GetRandomScripture();
+
+        while (!scripture.AreAllWordsHidden())
+        {
+            Console.Clear();
+            scripture.DisplayScripture();
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input == "quit")
+                break;
+
+            scripture.HideRandomWords();
+        }
+    }
+}
+
+class ScriptureLibrary
+{
+    private List<Scripture> scriptures;
+
+    public ScriptureLibrary()
+    {
+        scriptures = new List<Scripture>
+        {
+            new Scripture(new Reference("John 3:16"), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."),
+            new Scripture(new Reference("Genesis 1:1"), "In the beginning God created the heavens and the earth."),
+            // Add as many scriptures as you want here
+        };
+    }
+
+    public Scripture GetRandomScripture()
+    {
+        Random random = new Random();
+        int index = random.Next(scriptures.Count);
+        return scriptures[index];
+    }
+}
+
+class Scripture
+{
+    private Reference reference;
+    private string text;
+    private List<Word> words;
+
+    public Scripture(Reference reference, string text)
+    {
+        this.reference = reference;
+        this.text = text;
+        words = new List<Word>();
+
+        // Split the scripture text into words
+        string[] wordArray = text.Split(' ');
+
+        // Initialize Word objects for each word in the scripture text
+        foreach (string word in wordArray)
+        {
+            words.Add(new Word(word));
+        }
+    }
+
+    public void DisplayScripture()
+    {
+        Console.WriteLine(reference.GetReferenceString() + "\n");
+        foreach (Word word in words)
+        {
+            Console.Write(word.GetWord() + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void HideRandomWords()
+    {
+        Random random = new Random();
+
+        foreach (Word word in words)
+        {
+            if (!word.IsHidden() && random.Next(2) == 0)
+            {
+                word.Hide();
+            }
+        }
+    }
+
+    public bool AreAllWordsHidden()
+    {
+        foreach (Word word in words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
+    }
+}
+
 class Reference
 {
     private string referenceString;
@@ -48,7 +152,7 @@ class Reference
         }
     }
 
-    public string GetReferenceString()
+        public string GetReferenceString()
     {
         return referenceString;
     }
@@ -68,3 +172,34 @@ class Reference
         return verses;
     }
 }
+
+class Word
+{
+    private string word;
+    private bool hidden;
+
+    public Word(string word)
+    {
+        this.word = word;
+        hidden = false;
+    }
+
+    public string GetWord()
+    {
+        if (hidden)
+            return "___";
+        else
+            return word;
+    }
+
+    public bool IsHidden()
+    {
+        return hidden;
+    }
+
+    public void Hide()
+    {
+        hidden = true;
+    }
+}
+
