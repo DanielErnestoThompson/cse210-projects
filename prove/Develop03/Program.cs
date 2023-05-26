7c2e5b191cc77fe60e84c6bcba37580bd3b530f0
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-05-26 13:53:12 -0600

    added progress tracking capability

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,20 +6,31 @@ class Program
     static void Main()
     {
         ScriptureLibrary library = new ScriptureLibrary();
+        ProgressTracker tracker = new ProgressTracker();
 
-        Scripture scripture = library.GetRandomScripture();
-
-        while (!scripture.AreAllWordsHidden())
+        while (true)
         {
-            Console.Clear();
-            scripture.DisplayScripture();
-            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
-            string input = Console.ReadLine();
+            Scripture scripture = library.GetRandomScripture();
+            tracker.RecordAttempt(scripture);
 
-            if (input == "quit")
-                break;
+            while (!scripture.AreAllWordsHidden())
+            {
+                Console.Clear();
+                scripture.DisplayScripture();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+                string input = Console.ReadLine();
 
-            scripture.HideRandomWords();
+                if (input == "quit")
+                {
+                    if (scripture.AreAllWordsHidden())
+                        tracker.RecordSuccess(scripture);
+                    
+                    tracker.ReportProgress(scripture);
+                    break;
+                }
+
+                scripture.HideRandomWords();
+            }
         }
     }
 }
@@ -28,24 +39,14 @@ class ScriptureLibrary
 {
     private List<Scripture> scriptures;
 
-public ScriptureLibrary()
-{
-    scriptures = new List<Scripture>
+    public ScriptureLibrary()
     {
-        new Scripture(new Reference("John 3:16"), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."),
-        new Scripture(new Reference("Genesis 1:1"), "In the beginning God created the heavens and the earth."),
-        new Scripture(new Reference("Proverbs 3:5"), "Trust in the LORD with all your heart and lean not on your own understanding."),
-        new Scripture(new Reference("Psalm 23:1"), "The LORD is my shepherd, I lack nothing."),
-        new Scripture(new Reference("Matthew 28:19"), "Therefore go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit."),
-        new Scripture(new Reference("Romans 12:2"), "Do not conform to the pattern of this world, but be transformed by the renewing of your mind. Then you will be able to test and approve what God’s will is—his good, pleasing and perfect will."),
-        new Scripture(new Reference("Philippians 4:13"), "I can do all this through him who gives me strength."),
-        new Scripture(new Reference("John 14:6"), "Jesus answered, 'I am the way and the truth and the life. No one comes to the Father except through me.'"),
-        new Scripture(new Reference("Jeremiah 29:11"), "For I know the plans I have for you,” declares the LORD, “plans to prosper you and not to harm you, plans to give you hope and a future."),
-        new Scripture(new Reference("Psalm 46:1"), "God is our refuge and strength, an ever-present help in trouble."),
-        new Scripture(new Reference("Isaiah 40:31"), "But those who hope in the LORD will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.")
-    };
-}
-
+        scriptures = new List<Scripture>
+        {
+            new Scripture(new Reference("John 3:16"), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."),
+            // ... Add more scriptures here ...
+        };
+    }
 
     public Scripture GetRandomScripture()
     {
@@ -67,10 +68,7 @@ class Scripture
         this.text = text;
         words = new List<Word>();
 
-        // Split the scripture text into words
         string[] wordArray = text.Split(' ');
-
-        // Initialize Word objects for each word in the scripture text
         foreach (string word in wordArray)
         {
             words.Add(new Word(word));
@@ -90,7 +88,6 @@ class Scripture
     public void HideRandomWords()
     {
         Random random = new Random();
-
         foreach (Word word in words)
         {
             if (!word.IsHidden() && random.Next(2) == 0)
@@ -109,76 +106,25 @@ class Scripture
         }
         return true;
     }
+
+    public Reference GetReference()
+    {
+        return reference;
+    }
 }
 
 class Reference
 {
     private string referenceString;
-    private string book;
-    private int chapter;
-    private List<int> verses;
 
     public Reference(string referenceString)
     {
         this.referenceString = referenceString;
-        ParseReference();
     }
 
-    private void ParseReference()
-    {
-        string[] parts = referenceString.Split(':');
-
-        if (parts.Length == 2)
-        {
-            book = parts[0];
-            string[] chapterVerse = parts[1].Split(' ');
-
-            int chapterNumber;
-            if (int.TryParse(chapterVerse[0], out chapterNumber))
-            {
-                chapter = chapterNumber;
-            }
-
-            if (chapterVerse.Length > 1)
-            {
-                string[] verseParts = chapterVerse[1].Split(',');
-                verses = new List<int>();
-
-                foreach (string versePart in verseParts)
-                {
-                    int verseNumber;
-                    if (int.TryParse(versePart, out verseNumber))
-                    {
-                        verses.Add(verseNumber);
-                    }
-                }
-            }
-        }
-        else
-        {
-            // Handle invalid reference format
-            Console.WriteLine("Invalid reference format: " + referenceString);
-        }
-    }
-
-        public string GetReferenceString()
+    public string GetReferenceString()
     {
         return referenceString;
-    }
-
-    public string GetBook()
-    {
-        return book;
-    }
-
-    public int GetChapter()
-    {
-        return chapter;
-    }
-
-    public List<int> GetVerses()
-    {
-        return verses;
     }
 }
 
@@ -212,3 +158,45 @@ class Word
     }
 }
 
+class ProgressTracker
+{
+    private Dictionary<string, int> attempts;
+    private Dictionary<string, int> successes;
+
+    public ProgressTracker()
+    {
+        attempts = new Dictionary<string, int>();
+        successes = new Dictionary<string, int>();
+    }
+
+    public void RecordAttempt(Scripture scripture)
+    {
+        string refStr = scripture.GetReference().GetReferenceString();
+        if (attempts.ContainsKey(refStr))
+            attempts[refStr]++;
+        else attempts[refStr] = 1;
+    }
+
+    public void RecordSuccess(Scripture scripture)
+    {
+        string refStr = scripture.GetReference().GetReferenceString();
+        if (successes.ContainsKey(refStr))
+            successes[refStr]++;
+        else
+            successes[refStr] = 1;
+    }
+
+    public void ReportProgress(Scripture scripture)
+    {
+        string refStr = scripture.GetReference().GetReferenceString();
+        int attemptCount = attempts.ContainsKey(refStr) ? attempts[refStr] : 0;
+        int successCount = successes.ContainsKey(refStr) ? successes[refStr] : 0;
+
+        Console.WriteLine("\nScripture: " + refStr);
+        Console.WriteLine("Attempts: " + attemptCount);
+        Console.WriteLine("Successes: " + successCount);
+        Console.WriteLine("Success Rate: " + (successCount / (float)attemptCount * 100) + "%");
+    }
+}
+
+           
