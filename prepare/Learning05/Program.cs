8ef0d5c4dc9c1c11237a0f52ee79ff0665ec2f1c
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-06-15 21:16:11 -0600

    seperated into files, deleted duplicate classes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        // Create square
+        Square square = new Square("Red", 5.0);
+        shapes.Add(square);
+
+        // Create rectangle
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
+        shapes.Add(rectangle);
+
+        // Create circle
+        Circle circle = new Circle("Yellow", 3.0);
+        shapes.Add(circle);
+
+        // Display color and area of each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
