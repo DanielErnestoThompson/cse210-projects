4cea5cf6be406458698d83c86bbff61cf747d3ae
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-06-20 14:46:34 -0600

    added error handling

--- prove/Develop04/MindfulnessApp.cs
+++ prove/Develop04/MindfulnessApp.cs
@@ -1,4 +1,3 @@
-
 class MindfulnessApp
 {
     public void Run()
@@ -9,27 +8,48 @@ class MindfulnessApp
         while (true)
         {
             Console.WriteLine("Welcome to the Mindfulness App. Choose an activity:\n1. Breathing\n2. Reflection\n3. Listing\n0. Exit");
-            userChoice = int.Parse(Console.ReadLine());
+
+            // Error handling for userChoice
+            if (!int.TryParse(Console.ReadLine(), out userChoice))
+            {
+                Console.WriteLine("Invalid input! Please enter a number.");
+                continue;
+            }
 
             switch (userChoice)
             {
                 case 1:
                     Console.WriteLine("Enter the duration in seconds for the Breathing activity:");
-                    duration = int.Parse(Console.ReadLine());
+                    // Error handling for duration
+                    if (!int.TryParse(Console.ReadLine(), out duration))
+                    {
+                        Console.WriteLine("Invalid input! Please enter a number.");
+                        continue;
+                    }
                     BreathingActivity ba = new BreathingActivity(duration);
                     ba.PerformActivity();
                     break;
 
                 case 2:
                     Console.WriteLine("Enter the duration in seconds for the Reflection activity:");
-                    duration = int.Parse(Console.ReadLine());
+                    // Error handling for duration
+                    if (!int.TryParse(Console.ReadLine(), out duration))
+                    {
+                        Console.WriteLine("Invalid input! Please enter a number.");
+                        continue;
+                    }
                     ReflectionActivity ra = new ReflectionActivity(duration);
                     ra.PerformActivity();
                     break;
 
                 case 3:
                     Console.WriteLine("Enter the duration in seconds for the Listing activity:");
-                    duration = int.Parse(Console.ReadLine());
+                    // Error handling for duration
+                    if (!int.TryParse(Console.ReadLine(), out duration))
+                    {
+                        Console.WriteLine("Invalid input! Please enter a number.");
+                        continue;
+                    }
                     ListingActivity la = new ListingActivity(duration);
                     la.PerformActivity();
                     break;
